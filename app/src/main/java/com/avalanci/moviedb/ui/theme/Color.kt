2586717d3ea21cd0462784afc11b8f3b51141206
adamsmolik <adam@strv.com>
2021-08-19 17:37:42 +0200

    Update theme

--- app/src/main/java/com/avalanci/moviedb/ui/theme/Color.kt
+++ app/src/main/java/com/avalanci/moviedb/ui/theme/Color.kt
@@ -18,7 +18,9 @@ package com.avalanci.moviedb.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val LightBlue500 = Color(0xFF03A9F4)
+val LightBlue100 = Color(0xFFB3E5FC)
+val LightBlue700 = Color(0xFF0288D1)
+val Amber500 = Color(0xFFFFC107)
+val Amber700 = Color(0xFFFFA000)
+val Red500 = Color(0xFFF44336)
--- app/src/main/java/com/avalanci/moviedb/ui/theme/Theme.kt
+++ app/src/main/java/com/avalanci/moviedb/ui/theme/Theme.kt
@@ -5,17 +5,25 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
-	primary = Purple200,
-	primaryVariant = Purple700,
-	secondary = Teal200
+	primary = LightBlue100,
+	primaryVariant = LightBlue700,
+	secondary = Amber500,
+	secondaryVariant = Amber700,
+	error = Red500,
+	onError = Color.Black
 )
 
 private val LightColorPalette = lightColors(
-	primary = Purple500,
-	primaryVariant = Purple700,
-	secondary = Teal200
+	primary = LightBlue500,
+	onPrimary = Color.Black,
+	primaryVariant = LightBlue700,
+	secondary = Amber500,
+	secondaryVariant = Amber700,
+	error = Red500,
+	onError = Color.Black
 
 	/* Other default colors to override
     background = Color.White,
