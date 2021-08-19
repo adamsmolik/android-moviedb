40633f5506054941989b95eb749db89745ceded0
adamsmolik <adam@strv.com>
2021-08-19 17:51:24 +0200

    Remove unused import

--- app/src/main/java/com/avalanci/moviedb/ui/movies/composables.kt
+++ app/src/main/java/com/avalanci/moviedb/ui/movies/composables.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
