763dfeb402986502a329ec874517ec500e8a13d0
adamsmolik <adam@strv.com>
2021-08-23 14:30:37 +0200

    Update list progress composable

--- app/src/main/java/com/avalanci/moviedb/ui/common/ListProgress.kt
+++ app/src/main/java/com/avalanci/moviedb/ui/common/ListProgress.kt
@@ -1,8 +1,10 @@
 package com.avalanci.moviedb.ui.common
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
@@ -13,15 +15,13 @@ fun ListProgress(
 	modifier: Modifier = Modifier,
 	itemContent: @Composable() (brush: Brush) -> Unit
 ) {
-	LazyColumn(modifier = modifier) {
-		item {
-			Spacer(modifier = Modifier.height(16.dp))
-		}
+	val scrollState = rememberScrollState()
+
+	Column(modifier = modifier.verticalScroll(scrollState)) {
+		Spacer(modifier = Modifier.height(16.dp))
 		repeat(5) {
-			item {
-				ShimmerAnimation(itemContent)
-				Spacer(modifier = Modifier.height(16.dp))
-			}
+			ShimmerAnimation(itemContent)
+			Spacer(modifier = Modifier.height(16.dp))
 		}
 	}
 }
